1c3608da2f43501ec43064d4ad9eb81186b9f47c
okikiola11 <okikiola.apelehin@gmail.com>
2022-01-10 18:33:29 +0100

    fix: correct typos Remove console.logs from codes and the remaining comments

--- src/api.js
+++ src/api.js
@@ -4,7 +4,6 @@ const api = axios.create({
   baseURL: 'https://deckofcardsapi.com/api/deck/'
 });
 
-// create a new deck and shuffle
 const createNewDeckPlayerA = async () => {
   const { data } = await api.get(`new/shuffle`, {
     params: {
@@ -14,7 +13,6 @@ const createNewDeckPlayerA = async () => {
 
   const { deck_id: deckId } = data;
 
-  // draw a card from the deck for Player 1
   const { data: cardResponse } = await api.get(`${deckId}/draw/`, {
     params: {
       count: 1
@@ -24,7 +22,6 @@ const createNewDeckPlayerA = async () => {
   return {...cardResponse.cards[0], deckId }
 }
 
-// create a new deck and shuffle
 const createNewDeckPlayerB = async () => {
   const { data } = await api.get(`new/shuffle`, {
     params: {
@@ -34,29 +31,16 @@ const createNewDeckPlayerB = async () => {
 
   const { deck_id: PlayerBDeckId } = data;  
 
-  // draw a card from the deck for Player 1
   const { data: PlayerBCardResponse } = await api.get(`${PlayerBDeckId}/draw/`, {
     params: {
       count: 1
     }
   });
-  console.log(PlayerBCardResponse.cards);
+  
   const { image: PlayerBCardImageUrl, value: PlayerBCardValue  } = PlayerBCardResponse.cards[0];
   return { PlayerBCardImageUrl, PlayerBCardValue }
 
-  // console.log(PlayerBCardValue, PlayerBCardImageUrl);
 }
-
-// const drawCardFromDeck = async (deckId) => {
-//   const { data: cardResponse } = await api.get(`${deckId}/draw/`, {
-//     params: {
-//       count: 1
-//     }
-//   });
-
-//   consr { cards } = data;
-  
-// }
 
 export { createNewDeckPlayerA, createNewDeckPlayerB }
 
