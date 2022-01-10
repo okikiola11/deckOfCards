82ef4339e0eaa9ab45482b5b2be57b3d578d8a11
okikiola11 <okikiola.apelehin@gmail.com>
2022-01-10 16:34:58 +0100

    style: add the LayoutComponent Serve as the app container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-function App() {
-  return (
+import React from 'react';
+import { Header, Layout } from './LayoutComponents';
+
+import './App.css';
+
+const App = () => (
     <div className="App">
+      <Header />
+      <Layout />
     </div>
   );
-}
 
 export default App;
---
+++ src/LayoutComponents.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Header = styled.header`
+  width: 100%;
+  text-align: center;
+  background-color: green;
+  font-size: 24px;
+  color: #fefefe;
+  padding: 16px 0;
+`;
+
+export const CardLayout = styled.div`
+  display:  flex;
+  flex-direction: column;
+  margin: 15px;
+`;
